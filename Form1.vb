a012f893e30fc53baf52989cc5a7a85efd2223ac
BrooksHaasDoor <bsusor@haasdoor.com>
2023-07-27 15:32:44 -0400

    Format code

--- Form1.vb
+++ Form1.vb
@@ -7,6 +7,11 @@
     Dim description As String = ""
     Dim movieTitleList As New List(Of Object)
 
+    Private Sub MLMSForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddMovie("Barbie", 2023, "Adventure/Comedy/Fantasy", "Greta Gerwig", "Barbie suffers a crisis that leads her to question her world and her existence.")
+        AddMovie("The Shawshank Redemption", 1994, "Drama", "Frank Darabont", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.")
+    End Sub
+
     Private Sub AddMovieBtn_Click(sender As Object, e As EventArgs) Handles AddMovieBtn.Click
         AddMovie($"{MovieTitleTxt.Text}", $"{ReleaseYearTxt.Text}", $"{GenreTxt.Text}", $"{DirectorTxt.Text}", $"{DescriptionTxt.Text}")
         ResetInputs()
@@ -16,6 +21,7 @@
         ' Get the value of the selected cell and display it
         DisplayMovieList(MovieTitleDataGrid.Rows(e.RowIndex).Cells(e.ColumnIndex).Value)
     End Sub
+
     Private Sub SearchBtn_Click(sender As Object, e As EventArgs) Handles SearchBtn.Click
         ' Check if there is any data in MovieTitleDataGrid
         If MovieTitleDataGrid.Rows.Count = 0 Then
@@ -27,21 +33,19 @@
         DisplayMovieList(SearchTxt.Text)
     End Sub
 
+    Private Sub AddMovie(mov, rel, gen, dir, des)
+        ' Create and add a new movie to movieTitleList
+        movieTitleList.Add(CreateNewMovie(mov, rel, gen, dir, des))
+
+        ' Create a new row with value of mov
+        MovieTitleDataGrid.Rows.Add($"{mov}")
+    End Sub
+
     Private Function CreateNewMovie(mov, rel, gen, dir, des)
         ' Create a list of movie details
-        Dim newList As New List(Of String) From {
-                mov,
-                rel,
-                gen,
-                dir,
-                des
-            }
+        Dim newList As New List(Of String) From {mov, rel, gen, dir, des}
 
         Return newList
-    End Function
-
-    Private Function GetDisplayMessage() As String
-        Return $"Movie Title: {movieTitle}" & vbCrLf & $"Release Year: {releaseYear}" & vbCrLf & $"Genre: {genre}" & vbCrLf & $"Director: {director}" & vbCrLf & $"Description: {description}"
     End Function
 
     Private Sub DisplayMovieList(selectedMovie)
@@ -58,6 +62,8 @@
             End If
         Next
 
+        ' If the movie does not exist,
+        ' then display a different MessageBox and Exit Sub
         If (movieTitle = "" AndAlso releaseYear = "" AndAlso genre = "" AndAlso director = "" AndAlso description = "") Then
             MessageBox.Show("There are no movies with that title.")
             Exit Sub
@@ -67,16 +73,8 @@
         ResetGlobalVariables()
     End Sub
 
-    Private Sub AddMovie(mov, rel, gen, dir, des)
-        ' Create and add a new movie to movieTitleList
-        movieTitleList.Add(CreateNewMovie(mov, rel, gen, dir, des))
-
-        ' Create a new row with value of movieTitle
-        MovieTitleDataGrid.Rows.Add($"{mov}")
-    End Sub
-
     Private Sub ResetGlobalVariables()
-        ' Set globals to empty strings
+        ' Set global variable values to empty strings
         movieTitle = ""
         releaseYear = ""
         genre = ""
@@ -85,7 +83,7 @@
     End Sub
 
     Private Sub ResetInputs()
-        ' Set inputs to empty strings
+        ' Set input values to empty strings
         MovieTitleTxt.Text = ""
         ReleaseYearTxt.Text = ""
         GenreTxt.Text = ""
@@ -93,8 +91,7 @@
         DescriptionTxt.Text = ""
     End Sub
 
-    Private Sub MLMSForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        AddMovie("Barbie", 2023, "Adventure/Comedy/Fantasy", "Greta Gerwig", "Barbie suffers a crisis that leads her to question her world and her existence.")
-        AddMovie("The Shawshank Redemption", 1994, "Drama", "Frank Darabont", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.")
-    End Sub
+    Private Function GetDisplayMessage() As String
+        Return $"Movie Title: {movieTitle}" & vbCrLf & $"Release Year: {releaseYear}" & vbCrLf & $"Genre: {genre}" & vbCrLf & $"Director: {director}" & vbCrLf & $"Description: {description}"
+    End Function
 End Class
